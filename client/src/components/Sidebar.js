22c947e53818ce0aad0b2f68b799ea830d2ecf75
Polpy <emailparaoedu@gmail.com>
2021-01-31 17:51:56 +0000

    saving

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,41 +1,105 @@
-import React from 'react';
-import logo from '../style/logo/logo';
+import { Link } from 'react-router-dom';
 
-const Sidebar = () => {
-    return (
+import '../style/css/Sidebar.css';
+import logo from '../style/logo/logoJek.svg';
+import logomais from '../style/logo/logoplus.svg';
+import logopessoa from '../style/logo/logoman.svg';
+import logoarrowUP from '../style/logo/logoarrowUP.svg';
+import logoMoney from '../style/logo/logomoney.svg';
+
+function toggleNavTOT(x) {
+    let top = document.getElementById("menu").style.top;
+
+    if (top === "-200%"){
+        document.getElementById("menu").style.top = "100%";
+        document.getElementById("logoUP").style.transform = "rotate(0)";
+    }
+    else{
+        document.getElementById("menu").style.top = "-200%";
+        document.getElementById("logoUP").style.transform = "rotate(180deg)";
+    }
+    //alert(document.getElementById("menu").style.top);
+};
+
+function toggleNav(x){
+    let top = document.getElementById("transdivsMINHASDIVIDAS").style.top;
+
+    if (top === "-200%"){
+        document.getElementById("logoUP1").style.transform = "rotate(0deg)";
+        document.getElementById("dividastotaisdiv").style.top= "24%";
+        document.getElementById("transdivsMINHASDIVIDAS").style.top = "0%";
+    }
+    else{
+        document.getElementById("logoUP1").style.transform = "rotate(180deg)";
+        document.getElementById("dividastotaisdiv").style.top= "8%";
+        document.getElementById("transdivsMINHASDIVIDAS").style.top ="";
+    }
+}
+
+export const SideBAR = (props) => {
+    return(
         <div id ="sbbg">
             <img src={logo} className="App-logo" alt="logo" />
-            <div id = "criardivida">
-                <span id= "criardividaSTR">Criar dívida</span>
-            </div>
-            <div id = "mydividas">
-                <span id ="mydividasSTR">Minhas dívidas</span>
+                    <Link to="dividas/criar">
+                        <div id = "criardivida">
+                            <img src={logomais} className="App-logo-plus" alt="logoplus"/>
+                        <span id= "criardividaSTR">Criar dívida</span>
+                        </div>
+                    </Link>
+            
+            <div id = "mydividasdiv">
+                <div id="mydividas" onClick={toggleNav}>
+                    <img src={logopessoa} className="App-logo-pessoa" alt="logopess" />
+                    <span id ="mydividasSTR">Minhas dívidas</span>
 
-                <div id ="oqdevo">
-                    <span id ="oqdevoSTR">O que devo</span>
-                    
-                    <div id = "Ppagar">
-                        <span id = "PpagarSTR">Por pagar</span>
-                    </div>
-
-                    <div id="Pago">
-                        <span id = "PagoSTR"></span>
-                    </div>
+                    <img src={logoarrowUP} className="App-logo-UP" id="logoUP1" style={{transform: "rotate(180deg)"}}/>
                 </div>
-                <div id = "oqdevem"> 
-                    <span id ="oqdevemSTR">O que me devem</span>
 
-                    <div id = "Ppagar">
-                        <span id = "PpagarSTR">Por pagar</span>
-                    </div>
+                <div id= "transdivsMINHASDIVIDAS" style={{top: "-200%"}}>
+                    <Link to="dividas/minhasdividas">
+                        <div id ="oqdevo" >
+                            <span id ="oqdevoSTR">O que devo</span>  
+                        </div>
+                    </Link>
 
-                    <div id="Pago">
-                        <span id = "PagoSTR"></span>
-                    </div>
+                    <Link to="dividas/adever">
+                        <div id = "oqdevem">
+                            <span id = "oqdevemSTR">O que me devem</span>
+                        </div>
+                    </Link>
+                </div>
+                
+            </div>
+
+            <div id="dividastotaisdiv" style={{top: "8%"}}>
+                <div id= "dividastotais" onClick={toggleNavTOT}>
+                    <img src={logoMoney} className="App-logo-Money" alt="logomoney"/>
+                    <span id ="dividastotaisSTR">Dívidas Totais</span>
+
+                    <img src={logoarrowUP} className="App-logo-UP" id="logoUP" style={{transform: "rotate(180deg)"}}/>
+                </div>
+
+                <div id="menu" class="menu-hidden" style={{top: "-200%"}}>
+                    <Link to="dividas/Interno">
+                        <div id = "Interno"> 
+                            <span id ="InternoSTR">Interno</span>
+                        </div>
+                    </Link>
+                    
+                    <Link to="dividas/Inovacao">
+                        <div id = "Inovacao">
+                            <span id = "InovacaoSTR">Inovação</span>
+                        </div>
+                    </Link>
+
+                    <Link to="dividas/Tech">
+                        <div id = "Tech">
+                            <span id = "TechSTR">Tech</span>
+                        </div>
+                    </Link>
                 </div>
             </div>
         </div>
-    )
-}
-
-export default Sidebar;
+    );
+};
+export default SideBAR;
--- server/app.js
+++ server/app.js
@@ -7,65 +7,68 @@ const expressEjsLayout = require("express-ejs-layouts");
 const connectFlash = require('connect-flash');
 const session = require('express-session');
 const path = require('path');
-<<<<<<< Updated upstream:app.js
+
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
-=======
->>>>>>> Stashed changes:server/app.js
 
 const emails = require("./API/timer/SendingEmails"); //! apesar de não estar a ser usado, afirmar aqui para correr
 const dividasRoutes = require("./API/routes/dividas");
 const usersRoutes = require("./API/routes/users");
 const indexRoutes = require("./API/routes/index");
-const { Authenticator } = require("passport");
+const {
+  Authenticator
+} = require("passport");
 
 mongoose.connect(
-    "mongodb+srv://exp-node:givemmb@givemmb.aqww5.mongodb.net/exp-node?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  );
-  // ola
-  mongoose.Promise = global.Promise;
+  "mongodb+srv://exp-node:givemmb@givemmb.aqww5.mongodb.net/exp-node?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+// ola
+mongoose.Promise = global.Promise;
 
 
 
 app.set("view engine", "ejs");
-app.set("views","./views")
+app.set("views", "./views")
 
 app.use(cors());
 app.use(cookieParser('secret'));
 
 app.use(expressEjsLayout);
 app.use(morgan("dev"));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(bodyParser.json());
 app.use(express.json());
 
 
-app.use(express.static(path.join(__dirname,'/public')));
-app.use('public/stylesheets',express.static(path.join(__dirname,'public/stylesheets')));
+app.use(express.static(path.join(__dirname, '/public')));
+app.use('public/stylesheets', express.static(path.join(__dirname, 'public/stylesheets')));
 app.use(express.static("public"));
 
 
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 
 app.use(session({
-  secret : 'secret',
-  resave : true,
-  saveUninitialized : true
- }));
- //use flash
- app.use(connectFlash());
- app.use((req,res,next)=> {
-   res.locals.success_msg = req.flash('success_msg');
-   res.locals.error_msg = req.flash('error_msg');
-   res.locals.error  = req.flash('error');
- next();
- })
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}));
+//use flash
+app.use(connectFlash());
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+})
 
 // app.use((req,res,next) =>{
 //   res.header("Acess-Control-Allow-Origin","*");
@@ -80,29 +83,26 @@ app.use(session({
 app.use("/", indexRoutes);
 app.use("/users", usersRoutes);
 app.use("/dividas", dividasRoutes);
-<<<<<<< Updated upstream:app.js
 
 
 
-=======
->>>>>>> Stashed changes:server/app.js
 
 
 // ERROS
 //req = o que recebemos, res = resposta que damos
-app.use((req, res,next)=>{
-    const err = new Error("Not found.");
-    err.status = 404;
-    next(err);
+app.use((req, res, next) => {
+  const err = new Error("Not found.");
+  err.status = 404;
+  next(err);
 });
 
-app.use((err,req,res,next)=>{
-    const status = err.status || 500; //se nao existir o status do erro, envia 500
+app.use((err, req, res, next) => {
+  const status = err.status || 500; //se nao existir o status do erro, envia 500
 
-    //se exitir erro (por exemplo 404 se nao exitir) envia isto:
-    res.status(status).json({
-        message: 'Error not found! Status: ' + status
-    });
+  //se exitir erro (por exemplo 404 se nao exitir) envia isto:
+  res.status(status).json({
+    message: 'Error not found! Status: ' + status
+  });
 });
 
 module.exports = app;
