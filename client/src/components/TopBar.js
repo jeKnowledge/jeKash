66490bc657d591b23b923fb2a52fde318646da3a
Polpy <emailparaoedu@gmail.com>
2021-02-09 15:58:33 +0000

    Update TopBar.js

--- client/src/components/TopBar.js
+++ client/src/components/TopBar.js
@@ -1,20 +1,35 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import logoHamburguer from '../style/logo/logohamburguer.svg';
+import { Link, } from 'react-router-dom';
+import logoHamburguerSB from '../style/logo/logohamburguer.svg';
+import logoHamburguerN from '../style/logo/logoHamburguer2.svg'
 import logoJekL from '../style/logo/logoJekLetras.svg';
+import logoJekO from '../style/logo/logoJekLetrasColor.svg';
 import "../style/css/TopBar.css";
+let logoApp;
+let hamburguer;
 
 export const TopBar = (props) => {
+    const height = props.height; // da jeito adicionara height como parametro.
+    const linkto = props.linkto;
     const color = props.color;
-
+    
+    
+    if(props.logo === "whitebg") {
+        logoApp = logoJekL;
+        hamburguer = logoHamburguerSB;
+    }
+    else{
+        logoApp = logoJekO;
+        hamburguer = logoHamburguerN;
+    }
     return(
-        <div id ="bg" className="sidebarTopBar">
+        <div id ="bg" className={color} style = {{height: height}} >
 
-            <Link to="dividas/criar">
-                <img src={logoHamburguer} className="Hamburguer" alt="logoHamburguer" />
+            <Link to={linkto}>
+                <img src={hamburguer} className="Hamburguer" alt="logoHamburguer" />
             </Link>
             
-            <img src={logoJekL} className="App-logo" alt="logoJek" />
+            <img src={logoApp} className="AppLogo" alt="logoJek" />
 
         </div>
     );
