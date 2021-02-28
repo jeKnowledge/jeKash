9a6574f3a2f5d53459f15223546cac4cc703775e
Gonçalo Correia <gpcorreia>
2021-02-28 16:28:20 +0000

    Upgrade

--- client/src/components/Popup.js
+++ client/src/components/Popup.js
@@ -1,15 +1,30 @@
-import '../style/css/CriarDivida.css';
+import "../style/css/CriarDivida.css";
 
-const Popup = ({title, name1, name2, id1, id2}) => {
-
-    return ( 
-        <div className="popup">
-            <span className = {name1} id={id1}></span>
-            <span className= {name2} id={id2}>{title}</span>
+const Popup = ({ title, name1, name2, id1, id2, button }) => {
+  if (button) {
+    return (
+      <div className="popup">
+        <div>
+          <span className={name1} id={id1}></span>
+          <span className={name2} id={id2}>
+            {title}
+          </span>
         </div>
-     );
+        <div className="buttons-popup">
+          <button>Sim</button>
+          <button>Não</button>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div className="popup">
+      <span className={name1} id={id1}></span>
+      <span className={name2} id={id2}>
+        {title}
+      </span>
+    </div>
+  );
+};
 
-    
-}
- 
 export default Popup;
